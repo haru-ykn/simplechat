a27f34ec6c4be5595c825ec94fe5895fc432d2d6
haru-ykn <master_york_h@yahoo.co.jp>
2025-04-29 02:41:27 +0900

    Update index.py

--- lambda/index.py
+++ lambda/index.py
@@ -28,7 +28,7 @@ bedrock_client = None
 
 # モデルID
 #MODEL_ID = os.environ.get("MODEL_ID", "us.amazon.nova-lite-v1:0")
-MODEL_ID = "https://f6b4-34-16-246-87.ngrok-free.app"
+MODEL_ID = "https://2d66-34-125-213-245.ngrok-free.app"
 
 def lambda_handler(event, context):
     try:
